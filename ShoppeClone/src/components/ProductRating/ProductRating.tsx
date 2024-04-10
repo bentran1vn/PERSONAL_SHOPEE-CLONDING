ba655215bcf5a463584210a00c41a1a6f4f205b5
bentran1vn <tan182205@gmail.com>
2024-04-11 02:25:33 +0700

    Finish UI for Product Detail

--- ShoppeClone/src/components/ProductRating/ProductRating.tsx
+++ ShoppeClone/src/components/ProductRating/ProductRating.tsx
@@ -1,7 +1,13 @@
 interface Props {
   rating: number
+  activeClassName?: string
+  nonActiveClassName?: string
 }
-export default function ProductRating({ rating }: Props) {
+export default function ProductRating({
+  rating,
+  activeClassName = 'w-3 h-3 fill-yellow-300 text-yellow-300',
+  nonActiveClassName = 'w-3 h-3 fill-current text-gray-300'
+}: Props) {
   const handleWith = (order: number) => {
     if (order <= rating) {
       return '100%'
@@ -18,13 +24,7 @@ export default function ProductRating({ rating }: Props) {
         .map((_, index) => (
           <div className='relative' key={index}>
             <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: handleWith(index + 1) }}>
-              <svg
-                enableBackground='new 0 0 15 15'
-                viewBox='0 0 15 15'
-                x={0}
-                y={0}
-                className='w-3 h-3 fill-yellow-300 text-yellow-300'
-              >
+              <svg enableBackground='new 0 0 15 15' viewBox='0 0 15 15' x={0} y={0} className={activeClassName}>
                 <polygon
                   points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
                   strokeLinecap='round'
@@ -34,13 +34,7 @@ export default function ProductRating({ rating }: Props) {
               </svg>
             </div>
             <div>
-              <svg
-                enableBackground='new 0 0 15 15'
-                viewBox='0 0 15 15'
-                x={0}
-                y={0}
-                className='w-3 h-3 fill-current text-gray-300'
-              >
+              <svg enableBackground='new 0 0 15 15' viewBox='0 0 15 15' x={0} y={0} className={nonActiveClassName}>
                 <polygon
                   points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
                   strokeLinecap='round'
